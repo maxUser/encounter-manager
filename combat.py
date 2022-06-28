d761a880f95bfb451b00fcf097d7974248f3d993
Maximilian Kasprzik <Maximilian.Kasprzik@islandhealth.ca>
2022-06-28 16:00:20 -0700

    working on tiebreaker

--- combat.py
+++ combat.py
@@ -3,12 +3,13 @@ import sys
 from random import randint
 
 class Character:
-    def __init__(self, name, initiative):
+    def __init__(self, name, initiative, dex_bonus=0):
         self.name = name
         self.initiative = initiative
+        self.dex_bonus = dex_bonus
         
     def print_character(self):
-        print(self.name, self.initiative)
+        print(self.name, self.initiative, self.dex_bonus)
         
     def __str__(self):
         return self.name
@@ -36,7 +37,7 @@ def player_characters():
     # pierre_init = input('Pierre initiative: ')
     
     #return [Character('Dame Romaine', dame_init), Character('Dennis Le Menace', dennis_init), Character('Ronan', ronan_init), Character('Pierre', pierre_init)]
-    return [Character('Dame Romaine', 14), Character('Dennis Le Menace', 7), Character('Ronan', 21), Character('Pierre', 8)]
+    return [Character('Dame Romaine', 7, -2), Character('Dennis Le Menace', 7, 2), Character('Ronan', 21, 1), Character('Pierre', 21, 1)]
     
    
 #f = open('test_combat.txt', 'r')   
@@ -67,11 +68,9 @@ def nonplayer_characters():
             for i in range(num_npc):
                 # add consecutive numbers after each npc name
                 npc_multi = npc_name + str(i+1)
-                init = get_npc_init(dex_bonus)
-                npc_list.append(Character(npc_multi, init))
+                npc_list.append(Character(npc_multi, get_npc_init(dex_bonus), dex_bonus))
         else:
-            init = get_npc_init(dex_bonus)
-            npc_list.append(Character(npc_name, init))   
+            npc_list.append(Character(npc_name, get_npc_init(dex_bonus), dex_bonus))   
     
     return npc_list
 
@@ -134,13 +133,42 @@ def round_order(combatants):
 
 def tiebreaker(combatants):
     tie_exists = False
+    order = [x for x in combatants]
     for combatantX in combatants:
         for combatantY in combatants:
             if combatantX.initiative == combatantY.initiative and combatantX.name != combatantY.name:
                 tie_exists = True
     print('tie: ', tie_exists)
     if tie_exists is True:
-        print('tie')
+        # Get the value of the tied initiative scores and the characters with those scores
+        tied_combatants = {} # {17: [('Pierre', 0), ('Ronan', 1)], 4: [('Dame Romaine', 2), ('Dennis', 3)]}
+        for combatantX in combatants:
+            for combatantY in combatants:
+                if combatantX.initiative == combatantY.initiative:
+                    # Get all tied initiative scores, store them as keys
+                    tied_combatants[combatantX.initiative] = []
+        for combatant in combatants:
+            if combatant.initiative in tied_combatants:
+                # Add combatants as values (combatant, index) to respective initiative key
+                tied_combatants[combatant.initiative].append((combatant, order.index(combatant)))
+        
+        for init in tied_combatants.keys():
+            #print(tied_combatants[init][0][1], tied_combatants[init][-1][1])
+            #order[tied_combatants[init][0][1]:tied_combatants[init][-1][1]] = order[tied_combatants[init][0][1]:tied_combatants[init][-1][1]].sort(key=lambda x:x[, reverse=True)
+            # JUST MANUALLY SORT
+        print()
+        print()
+            
+        
+        
+            
+        
+        for k, v in tied_combatants.items():
+            print()
+            print('init: ', k)
+            for tup in v:
+                tup[0].print_character()
+                print('ind: ', tup[1])
     else:
         return combatants
     
@@ -152,7 +180,7 @@ def main():
     print_combatants(combatants)
     combatants = tiebreaker(combatants)
     
-    round_order(combatants)
+    #round_order(combatants)
     try:
         f.close()
     except:
